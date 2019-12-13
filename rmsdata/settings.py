d57468b8997e5f82fb4700b24607690d3afe54a5
zyl <zylzlh0330@163.com>
2019-12-13 14:03:51 +0800

    visualization

--- rmsdata/settings.py
+++ rmsdata/settings.py
@@ -87,13 +87,11 @@ WSGI_APPLICATION = 'rmsdata.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-        'NAME': '5g1',
-        'USER': 'postgres',
-        'PASSWORD': 'zz140330',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'rmsdata_postsql_V2',
+        'USER':'postgres',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
