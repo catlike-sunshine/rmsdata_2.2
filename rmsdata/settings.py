1c69fa4309b5643751dc86ea8df40f6e7d6b3145
Integrated Analysis Club <48879945+catlike-sunshine@users.noreply.github.com>
2020-01-14 15:43:19 +0800

    reset state of settings.py

--- rmsdata/settings.py
+++ rmsdata/settings.py
@@ -86,15 +86,12 @@ WSGI_APPLICATION = 'rmsdata.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': '5G',
-        #'USER':'postgres',
-        #'PASSWORD':'f7i9ep2h',
-        #'HOST':'localhost',
-        #'PORT':'5432',
-
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '5G',
+        'USER':'postgres',
+        'PASSWORD':'f7i9ep2h',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
